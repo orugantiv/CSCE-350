bb70f0da80763aa806e6d87401bfc220382f6404
Anirudh Oruganti <orugantiv@email.sc.edu>
2021-10-25 12:14:36 -0400

    QuickSort Done with make file

---
+++ Assignments/Project_1/inc/QuickSort.h
@@ -0,0 +1,104 @@
+/**
+ * Programmer Name: V.N. Anriudh Oruganti
+ * Class: CSCE 350
+ * Application:
+ * Excution:
+ * Copyright 2021 V.N. Anirudh Oruganti
+ **/
+#ifndef INC_QUICKSORT_H_
+#define INC_QUICKSORT_H_
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+class FileReadWrite {
+ public:
+  // This method read in the input from a given path of file and saves the
+  // values to vector.
+  std::vector<float> readFile(std::string path) {
+    std::string temp;
+    std::fstream dataFile;
+    dataFile.open(path.c_str());
+    assert(dataFile.is_open() && "***Could Not Open The File***");
+    while (dataFile >> temp) _dataList.push_back(std::stof(temp));
+    return _dataList;
+  }
+
+  void setDataVector(std::vector<float> newDataVector) {
+    _dataList = newDataVector;
+  }
+  std::vector<float> getDataVector() { return _dataList; }
+
+ private:
+  std::vector<float> _dataList;
+};
+class QuickSort {
+ public:
+  QuickSort(std::vector<float> DataVector) { _dataList = DataVector; }
+  void swap(int leftIndex, int rightIndex) {
+    float temp = 0;
+    temp = _dataList[leftIndex];
+    _dataList[leftIndex] = _dataList[rightIndex];
+    _dataList[rightIndex] = temp;
+  }
+  void doSort(int firstIndex, int lastIndex) {
+    int middleIndex = (firstIndex + lastIndex) / 2;
+    int newIndex = 0;
+
+    if (_dataList[lastIndex] < _dataList[middleIndex])
+      swap(lastIndex, middleIndex);
+    if (_dataList[middleIndex] < _dataList[firstIndex])
+      swap(middleIndex, firstIndex);
+    if (_dataList[lastIndex] < _dataList[middleIndex])
+      swap(middleIndex, lastIndex);
+
+    if (lastIndex - firstIndex <= 2)
+      return;
+
+    else {
+      swap(middleIndex, lastIndex - 1);
+      newIndex =
+          doPartition(firstIndex, lastIndex - 1, _dataList[lastIndex - 1]);
+      doSort(firstIndex, (newIndex - 1));
+      doSort((newIndex + 1), lastIndex);
+    }
+  }
+  int doPartition(int firstIndex, int lastIndex, float pivot) {
+    int pivotInitialIndex = lastIndex;
+    if (_dataList[firstIndex] < pivot)
+      doPartition(++firstIndex, lastIndex, pivot);
+    else if (_dataList[lastIndex] > pivot)
+      doPartition(firstIndex, --lastIndex, pivot);
+    else if (firstIndex >= lastIndex) {
+      swap(firstIndex, pivotInitialIndex);
+      return firstIndex;
+    } else {
+      swap(firstIndex, lastIndex);
+      doPartition(firstIndex, lastIndex, pivot);
+    }
+
+    // while (true)
+    // {
+    //     while (_dataList[++firstIndex] < pivot)
+    //         ;
+    //     while (_dataList[--lastIndex] > pivot)
+    //         ;
+    //     if (firstIndex >= lastIndex)
+    //         break;
+    //     swap(firstIndex, lastIndex);
+    // }
+
+    //  swap(firstIndex, pivotInitialIndex);
+  }
+
+  // returns a vector that contains list of floats
+  std::vector<float> getDataVector() { return _dataList; }
+
+ private:
+  std::vector<float> _dataList;
+  std::string _dataString;
+};
+
+#endif  // INC_QUICKSORT_H_
--- Assignments/Project_1/src/QuickSortAlgorithm.cpp
+++ Assignments/Project_1/src/QuickSortAlgorithm.cpp
@@ -2,5 +2,28 @@
  * Programmer Name: V.N. Anriudh Oruganti
  * Class: CSCE 350
  * Application:
- * Excution: 
- * /
+ * Excution:
+ * Copyright 2021 V.N. Anirudh Oruganti
+ **/
+
+// Includes
+#include <Project_1/inc/QuickSort.h>
+
+int main(int argc, char *argv[]) {
+  // Makes sure input is length is 3.
+  assert(argc == 3 && "Incorrect Number of Arguments ");
+  FileReadWrite file;
+
+  std::vector<float> dataList = file.readFile(argv[1]);
+
+  QuickSort sort(dataList);
+  for (int i = 0; i < dataList.size(); ++i) std::cout << dataList[i] << ' ';
+
+  sort.doSort(0, dataList.size() - 1);
+
+  dataList = sort.getDataVector();
+  std::cout << '\n' << std::endl;
+
+  for (int i = 0; i < dataList.size(); ++i) std::cout << dataList[i] << ' ';
+  return 0;
+}
