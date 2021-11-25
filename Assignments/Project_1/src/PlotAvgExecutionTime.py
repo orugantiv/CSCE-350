92c52c914e71b7a3ffcac1657fd8cb01b8eb724b
V.N. Anirudh Oruganti <oruganti@email.sc.edu>
2021-11-25 00:09:36 -0500

    Read me and I am done

---
+++ Assignments/Project_1/src/PlotAvgExecutionTime.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = open('Oruganti_V.N.Anirudh_averageExecutionTime.txt','r')
+fig,ax=plt.subplots()
+labels = file.readline()
+labels = labels.split('\t')
+dataInput = []
+dataTime = []
+while(1):
+    line = file.readline().replace("\n",'')
+    if line == '':
+        break;
+    else:
+        line = line.split('\t')
+        dataInput.append(float(line[0]))
+        dataTime.append(float(line[1]))
+
+ax.plot(dataInput, dataTime, marker="o")
+ax.set_title(labels[0]+' v. '+labels[1])
+ax.set_xlabel(labels[0])
+ax.set_ylabel(labels[1])
+plt.savefig('Oruganti_V.N.Anirudh_plotAverageExecutionTime.jpg')
